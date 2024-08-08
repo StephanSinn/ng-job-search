7ae3e6dc3408b149e32b76b31ce3a15a1ed35bda
Stephan Sinn <stephan.sinn@d-fine.com>
2024-08-08 10:30:36 +0200

    feat: show favorites

--- src/app/components/favorites-list/favorites-list.component.ts
+++ src/app/components/favorites-list/favorites-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { JobsDataService } from '../../services/jobs-data.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { JobItemComponent } from '../job-item/job-item.component';
 
 @Component({
   selector: 'app-favorites-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JobItemComponent, NgIf],
   templateUrl: './favorites-list.component.html',
   styles: ``,
 })
-export class FavoritesListComponent {}
+export class FavoritesListComponent {
+  jobDataService = inject(JobsDataService);
+}
